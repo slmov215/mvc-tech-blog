9fabf37a9280ea085ad88196ac53a63dd9594601
Stephen <127278005+slmov215@users.noreply.github.com>
2023-06-27 00:01:48 -0400

    Worked on seeds issues and json

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -42,6 +42,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+var distDir = __dirname + "/dist/";
+
+app.use(express.static(distDir));
 app.use(routes);
 
 // Syncs sequelize with database
